b383f520fd95086c6e367defc001281ed14ed314
mscalel <msksaul@gmail.com>
2024-05-08 14:08:56 -0600

    Total Count Cards

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,46 @@
-import { DealsChart, UpcomingEvents } from '@/components'
+import { DashboardTotalCount, DealsChart, UpcomingEvents } from '@/components'
+import { DASHBOARD_TOTAL_COUNTS_QUERY } from '@/graphql/query'
+import { DashboardTotalCountsQuery } from '@/graphql/types'
+import { useCustom } from '@refinedev/core'
 import { Col, Row } from 'antd'
 
 
 export const Home = () => {
+
+  const { data, isLoading } = useCustom<DashboardTotalCountsQuery>({
+    url: '',
+    method: 'get',
+    meta: {
+      gqlQuery: DASHBOARD_TOTAL_COUNTS_QUERY
+    }
+  })
+
   return (
     <div>
+      <Row gutter={[32, 32]}>
+        <Col xs={24} sm={24} xl={8}>
+          <DashboardTotalCount 
+            resource='companies'
+            isLoading={isLoading}
+            totalCount={data?.data.companies.totalCount}
+          />
+        </Col>
+        <Col xs={24} sm={24} xl={8}>
+          <DashboardTotalCount 
+            resource='contacts'
+            isLoading={isLoading}
+            totalCount={data?.data.contacts.totalCount}
+          />
+        </Col>
+        <Col xs={24} sm={24} xl={8}>
+          <DashboardTotalCount 
+            resource='deals'
+            isLoading={isLoading}
+            totalCount={data?.data.deals.totalCount}
+          />
+        </Col>
+      </Row>
+
       <Row
         gutter={[32, 32]}
         style={{
@@ -24,7 +60,7 @@ export const Home = () => {
         <Col
           xs={24}
           sm={24}
-          xl={8}
+          xl={16}
           style={{
             height: '460px'
           }}
