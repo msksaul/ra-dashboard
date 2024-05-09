b66b18eb746d3e6500079872dfd71f6bce1768eb
mscalel <msksaul@gmail.com>
2024-05-09 16:01:59 -0600

    Edit Company

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,8 @@ import { App as AntdApp } from "antd";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import Layout from './components/layout';
 import { resources } from './config/resources';
+import Create from './pages/company/create';
+import Edit from './pages/company/edit';
 
 function App() {
   return (
@@ -54,7 +56,11 @@ function App() {
                     }
                   >
                     <Route index element={<Home />}/>
-                    <Route path='/companies' element={<CompanyList />}/>
+                    <Route path='/companies'>
+                      <Route index element={<CompanyList />}/>
+                      <Route path='new' element={<Create/>}/>
+                      <Route path='edit/:id' element={<Edit/>}/>
+                    </Route>
                   </Route>
                 </Routes>
                 <RefineKbar />
